1092e9cf9f2e23ff99e7e231825f894a7710aa60
dlachausse <dlachausse@gmail.com>
2016-01-20 20:39:55 +0000

    Added X5/x5_5.cpp

---
+++ X5/x5_5.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+    std::string name;
+    int age;
+    
+    std::cout << "What is your name? ";
+    std::cin >> name;
+    
+    std::cout << "What is your age?";
+    std::cin >> age;
+    
+    std::cout << "Greetings " << name << ", " << age << " is a great age to be!\n";
+}
