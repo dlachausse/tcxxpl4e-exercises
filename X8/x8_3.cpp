d20cab0fbbbbbcbec41231d07ec81f771c029e3a
Darren LaChausse <dlachausse@gmail.com>
2016-02-27 22:58:44 -0500

    Added X8/x8_3.cpp

---
+++ X8/x8_3.cpp
@@ -0,0 +1,12 @@
+int main()
+{
+	using UChar = unsigned char;
+	using CUChar = const unsigned char;
+	using PInt = int*;
+	using PPChar = char**;
+	using PAChar = char*[];
+	using A7PInt = int*[7];
+	using PA7PInt = int*(*)[7];
+	using A8PInt7 = int(*)[7][8];
+}
+
